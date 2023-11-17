4392593823e89e7d9b609ea70133582b8773b748
Bluedyson <bluedyson@qq.com>
2023-11-17 20:43:00 +0800

    Config vitepress and deploy

--- issues/.vitepress/config.mjs
+++ issues/.vitepress/config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Blog",
-  base: '/issues/',
+  base: '/Press/',
   description: "A blog",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
