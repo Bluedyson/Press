8884fd7064ad069b329b80fa6e96836614f2c097
Bluedyson <97047955+Bluedyson@users.noreply.github.com>
2023-11-19 00:29:30 +0800

    Update public.js

--- issues/public/H5/public.js
+++ issues/public/H5/public.js
@@ -2,17 +2,17 @@ mySwiper1 = ""
 var imgs_1 = []
 for (let i = 0; i < 192; i++) {
     imgs_1[i] = new Image;
-    imgs_1[i].src = "/Press/H5/asset/qhy/" + 'qhy_' + padding(i,3) + ".webp";
+    imgs_1[i].src = "/H5/asset/qhy/" + 'qhy_' + padding(i,3) + ".webp";
 }
 var imgs_2 = []
 for (let i = 0; i < 477; i++) {
     imgs_2[i] = new Image;
-    imgs_2[i].src = "/Press/H5/asset/wk/" + 'wk_' + padding(i,3) + ".webp";
+    imgs_2[i].src = "/H5/asset/wk/" + 'wk_' + padding(i,3) + ".webp";
 }
 var imgs_3 = []
 for (let i = 0; i < 606; i++) {
     imgs_3[i] = new Image;
-    imgs_3[i].src = "/Press/H5/asset/zgf/" + 'zgf_' + padding(i,3) + ".webp";
+    imgs_3[i].src = "/H5/asset/zgf/" + 'zgf_' + padding(i,3) + ".webp";
 }
 
 window.onload = function () {
